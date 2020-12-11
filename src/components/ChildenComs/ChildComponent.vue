8bb196caf04963b28070bdd089e2d5618a161362
chongqianqian <yunle@dtstack.com>
2020-12-11 10:41:26 +0800

    测试model

---
+++ src/components/ChildenComs/ChildComponent.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <input :value="value" @input="onInputChange" v-bind="$attrs" autocomplete="off"/>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      type: String,
+      default: '我是子组件默认的props数据'
+    }
+  },
+  methods: {
+    onInputChange (e) {
+      this.$emit('input', e.target.value)
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,26 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+    下面是v-model示例：
+    <p>{{ pageTitle }}</p>
+
+    下面是子组件内容：
+    <ChildComponent v-model="pageTitle"  placeholder="今日待办" autofocus></ChildComponent>
   </div>
 </template>
+
+<script>
+import ChildComponent from '@/components/ChildenComs/ChildComponent.vue'
+export default {
+  data () {
+    return {
+      pageTitle: '我是来自父组件的字符串'
+    }
+  },
+  components: {
+    ChildComponent
+  }
+}
+</script>
+
+<style scoped>
+</style>
